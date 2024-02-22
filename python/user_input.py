23bff68377c75ab92e9889664924aeb3ea88fe7b
BrianKimurgor <kimurgorbrian20@gmail.com>
2024-02-22 10:20:41 +0300

    python input

---
+++ python/user_input.py
@@ -0,0 +1,10 @@
+"""
+ Write a program that asks the user for their name, age,
+ and location and then prints out a personalized message
+ """
+
+name = input('enter your name')
+age = input('enter your age')
+location = input('enter your location')
+
+print(f'hello {name} you are {age} years old and live in {location}')
